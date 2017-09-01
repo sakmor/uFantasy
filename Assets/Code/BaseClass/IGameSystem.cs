fa50cadcd58b5574d044b7d13be35f2222a97c95
王山姆 <sakmor@gmail.com>
2017-09-01 12:25:18 +0800

    Initial commit

---
+++ Assets/Code/BaseClass/IGameSystem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 遊戲子系統共用界面
+public abstract class IGameSystem
+{
+    protected mainGame mainGame = null;
+    public IGameSystem(mainGame mainGame)
+    {
+        this.mainGame = mainGame;
+    }
+
+    public virtual void Initialize() { }
+    public virtual void Release() { }
+    public virtual void Update() { }
+
+}
