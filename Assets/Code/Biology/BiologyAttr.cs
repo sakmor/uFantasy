624fc734d8829cb68fd2446480a36ffa8a42effa
sakmor <sakmor@gmail.com>
2017-10-26 17:53:44 +0800

    生物Level表資料讀取完成一版

--- Assets/Code/Biology/BiologyAttr.cs
+++ Assets/Code/Biology/BiologyAttr.cs
@@ -5,37 +5,82 @@ using UnityEngine;
 public class BiologyAttr : MonoBehaviour
 {
     private int _Hp = 1, _Def = 2, _Atk = 3, _ASpeed = 4, _Mp = 5, _MSpeed = 6, _Speed = 6;
-    public int Hp, Mp = 100;
-    public int HpMax, MpMax = 100;
-    private string Lv;
-    private uFantasy.Enum.BiologyType Type;
+    private int TypeStep = 7;
+    public string Lv;
+    public int Hp, HpMax, Def, Atk, ASpeed, Mp, MpMax, MSpeed, Speed;
+
+    public uFantasy.Enum.BiologyType Type;
 
 
-    void Awake()
+    void Start()
     {
         Biology biology = GetComponent<Biology>();
         Lv = biology.Lv;
         Type = biology.Type;
-        // LoadDB(); fixme:寫到一半
+        LoadDB();
         Rest();
     }
 
     private void LoadDB()
     {
         if (Lv == "" || GameDB.Instance.Level.ContainsKey(Lv) == false) return;
-
-        Hp = GetHp();
+        TypeStep = ((int)Type - 1) * TypeStep;
+        HpMax = GetHpMax();
+        Def = GetDef();
+        Atk = GetAtk();
+        ASpeed = GetASpeed();
+        MpMax = GetMpMax();
+        MSpeed = GetMSpeed();
+        Speed = GetSpeed(); ;
     }
 
-    private int GetHp()
-    {   //fixme:先不管生物的type
-        return int.Parse(GameDB.Instance.Level[Lv][_Hp]);
+    private int GetSpeed()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_Speed + TypeStep]);
     }
 
-    void Rest()
+    private int GetMSpeed()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_MSpeed + TypeStep]);
+    }
+
+    private int GetMpMax()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_Mp + TypeStep]);
+    }
+
+    private int GetASpeed()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_ASpeed + TypeStep]);
+    }
+
+    private int GetAtk()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_Atk + TypeStep]);
+    }
+
+    private int GetDef()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_Def + TypeStep]);
+    }
+
+    private int GetHpMax()
+    {
+        return int.Parse(GameDB.Instance.Level[Lv][_Hp + TypeStep]);
+    }
+
+
+    private void Update()
+    {
+
+    }
+
+    private void Rest()
     {
         Hp = HpMax;
     }
+
+
 
 
 }
