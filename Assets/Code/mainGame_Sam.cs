7fe45eeb867aea33f9cd19313df0176f64c10724
sakmor <sakmor@gmail.com>
2017-11-17 18:34:48 +0800

    新增選取物件發光，但 B101 只有頭部發光

--- Assets/Code/mainGame_Sam.cs
+++ Assets/Code/mainGame_Sam.cs
@@ -9,6 +9,7 @@ public class mainGame_Sam : MonoBehaviour
     public Biology Leader; //fixme:暫時改為public的
     public Biology[] Biologys;
     private DotLine DotLine;
+    private HighlightsFX HighlightsFX;
 
     private void Awake()
     {
@@ -19,10 +20,18 @@ public class mainGame_Sam : MonoBehaviour
     {
         Leader = GameObject.Find("10001 騎士01").GetComponent<Biology>(); //fixme:暫時指定隊長
         DotLine = GameObject.Find("Line").GetComponent<DotLine>();
-
-
+        HighlightsFX = Camera.main.GetComponent<HighlightsFX>();
     }
+    void HighlightsFXAdd(GameObject n)
+    {
+        HighlightsFX.ClearOutlineData();
 
+        Renderer[] Renderer = n.GetComponentsInChildren<Renderer>();
+
+        List<Renderer> one = new List<Renderer>(Renderer);
+
+        HighlightsFX.AddRenderers(one);
+    }
     // Update is called once per frame
     private void Update()
     {
@@ -59,6 +68,7 @@ public class mainGame_Sam : MonoBehaviour
         {
             if (hit.transform.GetComponent<Biology>())
             {
+                HighlightsFXAdd(Leader.gameObject);
                 Leader.BiologyMovement.Stop();
                 Leader = hit.transform.GetComponent<Biology>();
                 return;
