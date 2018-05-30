6258748d4184c781b813140fc47e61aa4eee8ebf
sakmor <sakmor@gmail.com>
2018-05-30 18:11:44 +0800

    改掉手機版的判定中

--- Assets/Code/System/myDropdown.cs
+++ Assets/Code/System/myDropdown.cs
@@ -4,45 +4,38 @@ using UnityEngine;
 using UnityEngine.UI;
 public class myDropdown : MonoBehaviour
 {
-    public Dropdown Dropdown;
-    public float[] DropdownValue = { 1, 0.75f, 0.5f };
-    private int ScreenWidth;
-    private int ScreenHeight;
-    private UnityEngine.PostProcessing.PostProcessingBehaviour PostProcessingBehaviour;
-    void Start()
-    {
-        PostProcessingBehaviour = Camera.main.GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour>();
-        ScreenWidth = Screen.width;
-        ScreenHeight = Screen.height;
-        Dropdown.onValueChanged.AddListener(delegate
-        {
-            myDropdownValueChangedHandler(Dropdown);
-        });
-    }
-    void Destroy()
-    {
-        Dropdown.onValueChanged.RemoveAllListeners();
-    }
+	public Dropdown Dropdown;
+	public float[] DropdownValue = { 1, 0.75f, 0.5f };
+	private int ScreenWidth;
+	private int ScreenHeight;
+	private UnityEngine.PostProcessing.PostProcessingBehaviour PostProcessingBehaviour;
+	void Start()
+	{
+		PostProcessingBehaviour = Camera.main.GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour>();
+		ScreenWidth = Screen.width;
+		ScreenHeight = Screen.height;
+		Dropdown.onValueChanged.AddListener(delegate
+		{
+			myDropdownValueChangedHandler(Dropdown);
+		});
+	}
+	void Destroy()
+	{
+		Dropdown.onValueChanged.RemoveAllListeners();
+	}
 
-    private void myDropdownValueChangedHandler(Dropdown target)
-    {
-        float s = DropdownValue[target.value];
-        Screen.SetResolution(Mathf.FloorToInt(ScreenWidth * s), Mathf.FloorToInt(ScreenHeight * s), true);//fixme:應該在整個遊戲的進入點
-    }
+	private void myDropdownValueChangedHandler(Dropdown target)
+	{
+		float s = DropdownValue[target.value];
+		Screen.SetResolution(Mathf.FloorToInt(ScreenWidth * s), Mathf.FloorToInt(ScreenHeight * s), true);//fixme:應該在整個遊戲的進入點
+	}
 
-    public void SetDropdownIndex(int index)
-    {
-        Dropdown.value = index;
-    }
-    public void ToggleSSAO()
-    {
-        PostProcessingBehaviour.profile.ambientOcclusion.enabled = !PostProcessingBehaviour.profile.ambientOcclusion.enabled;
-    }
-    public void TogglePcControl()
-    {
-        mainGame_Sam mainGame_Sam = GameObject.Find("mainGame").GetComponent<mainGame_Sam>();
-        if (mainGame_Sam.IsPcControl == true) mainGame_Sam.SetPcControl();
-        if (mainGame_Sam.IsPcControl == false) mainGame_Sam.SetMobileControl();
-
-    }
+	public void SetDropdownIndex(int index)
+	{
+		Dropdown.value = index;
+	}
+	public void ToggleSSAO()
+	{
+		PostProcessingBehaviour.profile.ambientOcclusion.enabled = !PostProcessingBehaviour.profile.ambientOcclusion.enabled;
+	}
 }
