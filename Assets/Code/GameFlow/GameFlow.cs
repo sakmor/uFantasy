7220f4624c9ee2f8b205fc7e8b4e46275d5137e3
王山姆 <sakmor@gmail.com>
2018-06-02 13:12:59 +0800

    Loading

--- Assets/Code/GameFlow/GameFlow.cs
+++ Assets/Code/GameFlow/GameFlow.cs
@@ -1,17 +1,54 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public partial class GameFlow : MonoBehaviour
 {
-	public Animator Animator;
-	public static Animator Controller;
+    public GameObject LoadingMask;
+    public Animator Animator;
+    public static Animator Controller;
 
-	private void Awake()
-	{
-		Object.DontDestroyOnLoad(this);
-	}
-	private void Start()
-	{
-		Animator.enabled = true;
-		Controller = Animator;
-	}
+    private void Awake()
+    {
+        Object.DontDestroyOnLoad(this);
+    }
+    private void Start()
+    {
+        Animator.enabled = true;
+        Controller = Animator;
+    }
+
+    public void Loading(string sceneName)
+    {
+        StartCoroutine(LoadingSceneRealProgress(sceneName));
+    }
+
+    private void LoadingEnd()
+    {
+        Debug.Log(LoadingMask.GetComponent<Animator>().gameObject.name);
+        LoadingMask.GetComponent<Animator>().SetTrigger("IsFinished");
+    }
+
+    IEnumerator LoadingSceneRealProgress(string sceneName)
+    {
+        yield return null;
+        AsyncOperation sceneAO = SceneManager.LoadSceneAsync(sceneName);
+
+        // disable scene activation while loading to prevent auto load
+        sceneAO.allowSceneActivation = false;
+
+        while (!sceneAO.isDone)
+        {
+            if (sceneAO.progress >= 0.9f)
+            {
+                sceneAO.allowSceneActivation = true;
+                LoadingEnd();
+            }
+            Debug.Log(sceneAO.progress);
+            yield return null;
+        }
+    }
+
+
 }
--- Assets/Code/mainGame.cs
+++ Assets/Code/mainGame.cs
@@ -6,195 +6,196 @@ using UnityEngine.EventSystems;
 
 public class mainGame : MonoBehaviour
 {
-	public float ResolutionScale = 0.5f, ButtonProcessTime;
-	public SelectUnit SelectUnit;
-	public Biology[] Biologys; //紀錄場景上所有的生物
-	private DotLine DotLine;
-	private enum InputState { Down, Hold, Up, None }
-	private mainGame.InputState CurrentInputState, LastInputState;
-	public bool IsDrag;
-	private float DragDist = 30, Depth;
-	private CameraMovement CameraMovement;
+    public float ResolutionScale = 0.5f, ButtonProcessTime;
+    public SelectUnit SelectUnit;
+    public Biology[] Biologys; //紀錄場景上所有的生物
+    private DotLine DotLine;
+    private enum InputState { Down, Hold, Up, None }
+    private mainGame.InputState CurrentInputState, LastInputState;
+    public bool IsDrag;
+    private float DragDist = 30, Depth;
+    private CameraMovement CameraMovement;
 
 
 
-	[SerializeField] private Vector3 InputPos, _InputPos;
+    [SerializeField] private Vector3 InputPos, _InputPos;
 
-	private void Awake()
-	{
-		CurrentInputState = LastInputState = InputState.None;
-		Biologys = (Biology[])FindObjectsOfType(typeof(Biology));
-		Depth = Camera.main.farClipPlane;
-	}
-	// Use this for initialization
-	private void Start()
-	{
-		DotLine = GameObject.Find("Line").GetComponent<DotLine>();
-		CameraMovement = Camera.main.GetComponent<CameraMovement>();
-	}
+    private void Awake()
+    {
+        CurrentInputState = LastInputState = InputState.None;
+        Biologys = (Biology[])FindObjectsOfType(typeof(Biology));
+        Depth = Camera.main.farClipPlane;
+    }
+    // Use this for initialization
+    private void Start()
+    {
+        DotLine = GameObject.Find("Line").GetComponent<DotLine>();
+        CameraMovement = Camera.main.GetComponent<CameraMovement>();
+    }
 
-	// Update is called once per frame
-	private void Update()
-	{
-		InputProcess();
-		ButtonProcess();
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+        InputProcess();
+        ButtonProcess();
+    }
 
-	private void ButtonProcess()
-	{
-		if (!Input.anyKey) { ButtonProcessTime = 1; return; }
-		ButtonProcessTime += Time.deltaTime * 2;
-		if (Input.GetKey("w")) CameraMovement.OffsetCamTarget(Vector2.up, ButtonProcessTime * Time.deltaTime);
-		if (Input.GetKey("a")) CameraMovement.OffsetCamTarget(Vector2.left, ButtonProcessTime * Time.deltaTime);
-		if (Input.GetKey("s")) CameraMovement.OffsetCamTarget(Vector2.down, ButtonProcessTime * Time.deltaTime);
-		if (Input.GetKey("d")) CameraMovement.OffsetCamTarget(Vector2.right, ButtonProcessTime * Time.deltaTime);
-	}
+    private void ButtonProcess()
+    {
+        if (!Input.anyKey) { ButtonProcessTime = 1; return; }
+        ButtonProcessTime += Time.deltaTime * 2;
+        if (Input.GetKey("w")) CameraMovement.OffsetCamTarget(Vector2.up, ButtonProcessTime * Time.deltaTime);
+        if (Input.GetKey("a")) CameraMovement.OffsetCamTarget(Vector2.left, ButtonProcessTime * Time.deltaTime);
+        if (Input.GetKey("s")) CameraMovement.OffsetCamTarget(Vector2.down, ButtonProcessTime * Time.deltaTime);
+        if (Input.GetKey("d")) CameraMovement.OffsetCamTarget(Vector2.right, ButtonProcessTime * Time.deltaTime);
+    }
 
-	internal void SetMobileControl()
-	{
-		throw new NotImplementedException();
-	}
+    internal void SetMobileControl()
+    {
+        throw new NotImplementedException();
+    }
 
-	internal void SetPcControl()
-	{
-		throw new NotImplementedException();
-	}
+    internal void SetPcControl()
+    {
+        throw new NotImplementedException();
+    }
 
-	private bool IsPointerOverUIObject()
-	{
-		PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
-		eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-		List<RaycastResult> results = new List<RaycastResult>();
-		EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
-		return results.Count > 0;
-	}
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
 
-	private void InputProcess()
-	{
-		InputPosUpdate();
-		DragStateUpdate();
-		InputStateUpdate();
-
-
-		ScreenEdgeMove(10f);
-
-		InputNone();
-		InputHold();
-		InputDrag();
-		InputUp();
-		InputRightKeyUp();
-		InputDragUp();
-		InputDown();
-	}
-
-	private void InputRightKeyUp()
-	{
-		if (Input.GetMouseButtonUp(1)) SelectUnit.InputLeftKeyUp(GetInputRaycastHit());
-		// SelectUnit.InputRightKeyUp();
-	}
-
-	private void ScreenEdgeMove(float speed)
-	{
-		if (InputPos.x <= 0) Camera.main.GetComponent<CameraMovement>().targetOffset += Camera.main.transform.right * Time.deltaTime * speed;
-		if (InputPos.x >= Screen.width) Camera.main.GetComponent<CameraMovement>().targetOffset -= Camera.main.transform.right * Time.deltaTime * speed;
-		if (InputPos.y <= 0) Camera.main.GetComponent<CameraMovement>().targetOffset += Camera.main.transform.up * Time.deltaTime * speed;
-		if (InputPos.y >= Screen.height) Camera.main.GetComponent<CameraMovement>().targetOffset -= Camera.main.transform.up * Time.deltaTime * speed;
-	}
-
-	public Vector3 GetInputPos()
-	{
-		return InputPos;
-	}
-
-	private void InputDragUp()
-	{
-		if (CurrentInputState != InputState.Up || IsDrag == false) return;
-		SelectUnit.InputDragUp();
-	}
-
-	private void InputDrag()
-	{
-		if (CurrentInputState != InputState.Hold || IsDrag == false) return;
-		SelectUnit.InputDrag();
-	}
-
-	private void DragStateUpdate()
-	{
-		if (CurrentInputState == InputState.Down) _InputPos = InputPos;
-		if (CurrentInputState == InputState.Up) IsDrag = false;
-		if (CurrentInputState == InputState.Hold && Vector3.Distance(InputPos, _InputPos) > DragDist) IsDrag = true;
-	}
-
-	private void InputStateUpdate()
-	{
-		if (IsTouch() == true)
-		{
-			if (LastInputState == InputState.None || LastInputState == InputState.Up) CurrentInputState = InputState.Down;
-			if (LastInputState == InputState.Hold || LastInputState == InputState.Down) CurrentInputState = InputState.Hold;
-		}
-		if (IsTouch() == false)
-		{
-			if (LastInputState == InputState.Hold || LastInputState == InputState.Down) CurrentInputState = InputState.Up;
-			if (LastInputState == InputState.None || LastInputState == InputState.Up) CurrentInputState = InputState.None;
-		}
-		LastInputState = CurrentInputState;
-	}
-
-	private void InputDown()
-	{
-		if (CurrentInputState == InputState.Down)
-			SelectUnit.InputDown();
-	}
-	private void InputNone()
-	{
-		if (CurrentInputState != InputState.None) return;
-		SelectUnit.InputNone();
-
-	}
-	private void InputUp()
-	{
-		if (CurrentInputState != InputState.Up || IsDrag || false) return;
-		DotLine.DrawLineStop();
-	}
-	private void InputHold()
-	{
-		if (CurrentInputState != InputState.Hold && IsDrag == true) return;
-		SelectUnit.InputHold();
-	}
+    private void InputProcess()
+    {
+        InputPosUpdate();
+        DragStateUpdate();
+        InputStateUpdate();
 
 
-	private RaycastHit GetInputRaycastHit()
-	{
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(InputPos);
-		Physics.Raycast(ray, out hit, Depth);
-		return hit;
-	}
+        ScreenEdgeMove(10f);
 
-	private bool IsTouch()
-	{
-		if (Application.platform == RuntimePlatform.WindowsEditor ||
-			Application.platform == RuntimePlatform.WebGLPlayer)
-		{
-			if (Input.GetMouseButton(0)) return true;
-		}
-		if (Application.platform == RuntimePlatform.Android) if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary)) return true;
+        InputNone();
+        InputHold();
+        InputDrag();
+        InputUp();
+        InputRightKeyUp();
+        InputDragUp();
+        InputDown();
+    }
 
-		return false;
-	}
+    private void InputRightKeyUp()
+    {
+        if (Input.GetMouseButtonUp(1)) SelectUnit.InputLeftKeyUp(GetInputRaycastHit());
+        // SelectUnit.InputRightKeyUp();
+    }
 
-	public void InputPosUpdate()
-	{
-		if (Application.platform == RuntimePlatform.WindowsEditor ||
-			Application.platform == RuntimePlatform.WebGLPlayer)
-		{
-			InputPos = Input.mousePosition;
-		}
-		if (Application.platform == RuntimePlatform.Android)
-		{
-			InputPos = Input.GetTouch(0).position;
-		}
-	}
+    private void ScreenEdgeMove(float speed)
+    {
+        if (!Application.isFocused) return;
+        if (InputPos.x <= 0) Camera.main.GetComponent<CameraMovement>().targetOffset += Camera.main.transform.right * Time.deltaTime * speed;
+        if (InputPos.x >= Screen.width) Camera.main.GetComponent<CameraMovement>().targetOffset -= Camera.main.transform.right * Time.deltaTime * speed;
+        if (InputPos.y <= 0) Camera.main.GetComponent<CameraMovement>().targetOffset += Camera.main.transform.up * Time.deltaTime * speed;
+        if (InputPos.y >= Screen.height) Camera.main.GetComponent<CameraMovement>().targetOffset -= Camera.main.transform.up * Time.deltaTime * speed;
+    }
+
+    public Vector3 GetInputPos()
+    {
+        return InputPos;
+    }
+
+    private void InputDragUp()
+    {
+        if (CurrentInputState != InputState.Up || IsDrag == false) return;
+        SelectUnit.InputDragUp();
+    }
+
+    private void InputDrag()
+    {
+        if (CurrentInputState != InputState.Hold || IsDrag == false) return;
+        SelectUnit.InputDrag();
+    }
+
+    private void DragStateUpdate()
+    {
+        if (CurrentInputState == InputState.Down) _InputPos = InputPos;
+        if (CurrentInputState == InputState.Up) IsDrag = false;
+        if (CurrentInputState == InputState.Hold && Vector3.Distance(InputPos, _InputPos) > DragDist) IsDrag = true;
+    }
+
+    private void InputStateUpdate()
+    {
+        if (IsTouch() == true)
+        {
+            if (LastInputState == InputState.None || LastInputState == InputState.Up) CurrentInputState = InputState.Down;
+            if (LastInputState == InputState.Hold || LastInputState == InputState.Down) CurrentInputState = InputState.Hold;
+        }
+        if (IsTouch() == false)
+        {
+            if (LastInputState == InputState.Hold || LastInputState == InputState.Down) CurrentInputState = InputState.Up;
+            if (LastInputState == InputState.None || LastInputState == InputState.Up) CurrentInputState = InputState.None;
+        }
+        LastInputState = CurrentInputState;
+    }
+
+    private void InputDown()
+    {
+        if (CurrentInputState == InputState.Down)
+            SelectUnit.InputDown();
+    }
+    private void InputNone()
+    {
+        if (CurrentInputState != InputState.None) return;
+        SelectUnit.InputNone();
+
+    }
+    private void InputUp()
+    {
+        if (CurrentInputState != InputState.Up || IsDrag || false) return;
+        DotLine.DrawLineStop();
+    }
+    private void InputHold()
+    {
+        if (CurrentInputState != InputState.Hold && IsDrag == true) return;
+        SelectUnit.InputHold();
+    }
+
+
+    private RaycastHit GetInputRaycastHit()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(InputPos);
+        Physics.Raycast(ray, out hit, Depth);
+        return hit;
+    }
+
+    private bool IsTouch()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor ||
+            Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            if (Input.GetMouseButton(0)) return true;
+        }
+        if (Application.platform == RuntimePlatform.Android) if (Input.touchCount > 0 && (Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary)) return true;
+
+        return false;
+    }
+
+    public void InputPosUpdate()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor ||
+            Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            InputPos = Input.mousePosition;
+        }
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            InputPos = Input.GetTouch(0).position;
+        }
+    }
 
 
 }
